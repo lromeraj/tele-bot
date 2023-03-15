75cc53e19d7bda4c77a6ec75f94e22b9e7a3f249
lromeraj <lromerajdev@gmail.com>
2023-03-15 14:12:06 +0100

    FIX Storage dir

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
+import path from "path";
 import TelegramBot from "node-telegram-bot-api";
 import { LocalStorage } from "node-localstorage";
 
-const localStorage = new LocalStorage( './scratch/' );
+const DEFAULT_STORAGE_DIR = './tele-bot-storage';
+
+export interface TeleBotOptions {
+  token: string;
+  secret: string;
+  storageDir?: string;
+}
 
 interface GlobalContext {
   secret?: string,
   idOwnerChat?: string,
   bot: TelegramBot,
+  commands: { [key: string]: TelegramBot.BotCommand }
 }
 
+let localStorage = new LocalStorage( DEFAULT_STORAGE_DIR );
+
 const global: GlobalContext = {
+  commands: {},
   bot: new TelegramBot( '', { polling: false, webHook: false }),
 };
 
@@ -25,42 +36,71 @@ function botMessageHandler( msg: TelegramBot.Message ) {
   const argc = argv.length;
 
   if ( !global.idOwnerChat && cmd === '/owner' ) {
-
+    
     const idOwnerChat = msg.chat.id;
-
+    
     if ( argc === 1 && validSecret( argv[ 0 ] ) ) {
       setOwnerChatId( idOwnerChat );
       global.bot.sendMessage( idOwnerChat, `Owner OK @ ${ idOwnerChat }` );
     } else {
       global.bot.sendMessage( idOwnerChat, `Owner ERR` );
     }
-    
-  }
 
+    global.bot.deleteMessage( idOwnerChat, msg.message_id );
+  
+  }
+  
 }
 
-export function setup( token: string, secret: string ) {
+function applyCurrentCommands() {
+  return global.bot.setMyCommands(
+    Object.keys( global.commands )
+      .map( cmdKey => global.commands[ cmdKey ] ) );
+}
 
-  const idOwnerChat = localStorage.getItem('id')
+export function unsetCommands( commandKeys: string[] ) {
+  commandKeys.forEach( key => {
+    delete global.commands[ key ];
+  })
+  return applyCurrentCommands();
+}
 
-  if ( idOwnerChat ) {
-    global.idOwnerChat = idOwnerChat
-  }
+export function setCommands( commands:TelegramBot.BotCommand[] ) {
+  commands.forEach( cmd => {
+    global.commands[ cmd.command ] = cmd;
+  })
+  return applyCurrentCommands();
+}
 
-  global.secret = secret;
-  global.bot.removeAllListeners();
+export function setup( options: TeleBotOptions ) {
 
-  global.bot = new TelegramBot( token, { 
-    polling: true,
-  });
+  return new Promise( (resolve, reject) => {
+    
+    if ( options.storageDir ) {
+      localStorage = new LocalStorage( options.storageDir );
+    }
 
-  global.bot.setMyCommands([
-    { command: 'owner', description: 'Allows you to be assigned as a proprietary of the bot' },
-  ]);
+    const idOwnerChat = localStorage.getItem('id')
+
+    if ( idOwnerChat ) {
+      global.idOwnerChat = idOwnerChat
+    }
   
-  global.bot.on( 'message', botMessageHandler );
+    global.secret = options.secret;
+    global.bot.removeAllListeners();
+  
+    global.bot = new TelegramBot( options.token, { 
+      polling: true,
+    });
+    
+    global.bot.on( 'message', botMessageHandler );
+    
+    setCommands([
+      { command: 'owner', description: 'Set bot owner using self assignment' },
+    ]).then( () => resolve( global.bot ) ).catch( reject );
 
-  return global.bot;
+  })
+
 }
 
 export function getOwnerChatId( callback:( idChat: string ) => void ) {
@@ -70,7 +110,7 @@ export function getOwnerChatId( callback:( idChat: string ) => void ) {
 }
 
 export const validateEvent = ( 
-  callback: ( message: TelegramBot.Message, metadata: TelegramBot.Metadata ) => void 
+  callback: ( message: TelegramBot.Message, metadata: TelegramBot.Metadata ) => void
 ) => {
   return ( message: TelegramBot.Message, metadata: TelegramBot.Metadata ) => {
     if ( message.chat.id.toString() === global.idOwnerChat! ) {
