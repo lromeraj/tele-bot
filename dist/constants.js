659896145fe9479c8f83771ca750fe04f57c0f54
lromeraj <javierroll12@gmail.com>
2024-04-03 15:46:17 +0200

    npm run dist

--- dist/constants.js
+++ dist/constants.js
@@ -4,5 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DEFAULT_STORAGE_DIR = void 0;
+const os_1 = __importDefault(require("os"));
 const path_1 = __importDefault(require("path"));
-exports.DEFAULT_STORAGE_DIR = path_1.default.join('/tmp', `tele-bot-${process.pid}`);
+exports.DEFAULT_STORAGE_DIR = path_1.default.join(os_1.default.tmpdir(), `tele-bot-${process.pid}`);
